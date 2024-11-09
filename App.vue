990fd9270df8956383de89c4e9bd903f8a969c83
maulanayusuf93 <75455194+maulanayusuf93@users.noreply.github.com>
2024-11-09 08:50:43 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <div>
+      <h1>Counter</h1>
+      <span id="value">1</span>
+      <div>
+        <button id="decrease">-</button>
+        <button id="increase">+</button>
+      </div>
+    </div>
+  </main>
+</template>
